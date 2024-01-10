4723ad9cfe366de6ae4a70435b76fd266125d072
nosuz <nosuzuki@postcard.st>
2024-01-10 15:25:47 +0900

    Enable CRC check at accessing SD card.

--- src/hal/sd_driver.zig
+++ src/hal/sd_driver.zig
@@ -11,6 +11,7 @@ const SDError = error{
     ReadError,
     WriteError,
     CardError,
+    CrcError,
 };
 
 const CMD0 = [_]u8{ 0b01_000000 + 0, 0, 0, 0, 0, 0b1001010_1 }; // need correct CRC7 (0x4A << 1 | 1 = 0x95)
@@ -64,7 +65,55 @@ pub fn SD_DRIVER(comptime spi_port: anytype, comptime cs_pin: anytype) type {
             spi_port.write(&cmd_crc);
         }
 
+        const ccitt_hash = [_]u16{
+            0x0000, 0x1021, 0x2042, 0x3063, 0x4084, 0x50a5, 0x60c6, 0x70e7,
+            0x8108, 0x9129, 0xa14a, 0xb16b, 0xc18c, 0xd1ad, 0xe1ce, 0xf1ef,
+            0x1231, 0x0210, 0x3273, 0x2252, 0x52b5, 0x4294, 0x72f7, 0x62d6,
+            0x9339, 0x8318, 0xb37b, 0xa35a, 0xd3bd, 0xc39c, 0xf3ff, 0xe3de,
+            0x2462, 0x3443, 0x0420, 0x1401, 0x64e6, 0x74c7, 0x44a4, 0x5485,
+            0xa56a, 0xb54b, 0x8528, 0x9509, 0xe5ee, 0xf5cf, 0xc5ac, 0xd58d,
+            0x3653, 0x2672, 0x1611, 0x0630, 0x76d7, 0x66f6, 0x5695, 0x46b4,
+            0xb75b, 0xa77a, 0x9719, 0x8738, 0xf7df, 0xe7fe, 0xd79d, 0xc7bc,
+            0x48c4, 0x58e5, 0x6886, 0x78a7, 0x0840, 0x1861, 0x2802, 0x3823,
+            0xc9cc, 0xd9ed, 0xe98e, 0xf9af, 0x8948, 0x9969, 0xa90a, 0xb92b,
+            0x5af5, 0x4ad4, 0x7ab7, 0x6a96, 0x1a71, 0x0a50, 0x3a33, 0x2a12,
+            0xdbfd, 0xcbdc, 0xfbbf, 0xeb9e, 0x9b79, 0x8b58, 0xbb3b, 0xab1a,
+            0x6ca6, 0x7c87, 0x4ce4, 0x5cc5, 0x2c22, 0x3c03, 0x0c60, 0x1c41,
+            0xedae, 0xfd8f, 0xcdec, 0xddcd, 0xad2a, 0xbd0b, 0x8d68, 0x9d49,
+            0x7e97, 0x6eb6, 0x5ed5, 0x4ef4, 0x3e13, 0x2e32, 0x1e51, 0x0e70,
+            0xff9f, 0xefbe, 0xdfdd, 0xcffc, 0xbf1b, 0xaf3a, 0x9f59, 0x8f78,
+            0x9188, 0x81a9, 0xb1ca, 0xa1eb, 0xd10c, 0xc12d, 0xf14e, 0xe16f,
+            0x1080, 0x00a1, 0x30c2, 0x20e3, 0x5004, 0x4025, 0x7046, 0x6067,
+            0x83b9, 0x9398, 0xa3fb, 0xb3da, 0xc33d, 0xd31c, 0xe37f, 0xf35e,
+            0x02b1, 0x1290, 0x22f3, 0x32d2, 0x4235, 0x5214, 0x6277, 0x7256,
+            0xb5ea, 0xa5cb, 0x95a8, 0x8589, 0xf56e, 0xe54f, 0xd52c, 0xc50d,
+            0x34e2, 0x24c3, 0x14a0, 0x0481, 0x7466, 0x6447, 0x5424, 0x4405,
+            0xa7db, 0xb7fa, 0x8799, 0x97b8, 0xe75f, 0xf77e, 0xc71d, 0xd73c,
+            0x26d3, 0x36f2, 0x0691, 0x16b0, 0x6657, 0x7676, 0x4615, 0x5634,
+            0xd94c, 0xc96d, 0xf90e, 0xe92f, 0x99c8, 0x89e9, 0xb98a, 0xa9ab,
+            0x5844, 0x4865, 0x7806, 0x6827, 0x18c0, 0x08e1, 0x3882, 0x28a3,
+            0xcb7d, 0xdb5c, 0xeb3f, 0xfb1e, 0x8bf9, 0x9bd8, 0xabbb, 0xbb9a,
+            0x4a75, 0x5a54, 0x6a37, 0x7a16, 0x0af1, 0x1ad0, 0x2ab3, 0x3a92,
+            0xfd2e, 0xed0f, 0xdd6c, 0xcd4d, 0xbdaa, 0xad8b, 0x9de8, 0x8dc9,
+            0x7c26, 0x6c07, 0x5c64, 0x4c45, 0x3ca2, 0x2c83, 0x1ce0, 0x0cc1,
+            0xef1f, 0xff3e, 0xcf5d, 0xdf7c, 0xaf9b, 0xbfba, 0x8fd9, 0x9ff8,
+            0x6e17, 0x7e36, 0x4e55, 0x5e74, 0x2e93, 0x3eb2, 0x0ed1, 0x1ef0,
+        };
+
+        fn crc16(message: []const u8) u16 {
+            // CRC16-CCITT
+            // https://gist.github.com/rafacouto/59326c90d6a55f86a3ba
+            var crc: u16 = 0;
+            for (0..message.len) |i| {
+                crc = (crc << 8) ^ ccitt_hash[@as(u8, @truncate(((crc >> 8) ^ message[i]) & 0x00FF))];
+            }
+
+            return crc;
+        }
+
         pub fn init() SDError!void {
+            errdefer cleanup();
+
             time.sleep_ms(1);
             // _ = self;
 
@@ -85,6 +134,7 @@ pub fn SD_DRIVER(comptime spi_port: anytype, comptime cs_pin: anytype) type {
                 if ((response_r1[0] & 0x80) == 0) break;
                 if (i == 9) return SDError.InitError;
             }
+            if ((response_r1[0] & 0x7F) == 8) return SDError.CrcError;
 
             time.sleep_ms(1);
 
@@ -94,9 +144,19 @@ pub fn SD_DRIVER(comptime spi_port: anytype, comptime cs_pin: anytype) type {
                 if ((response_r1[0] & 0x80) == 0) break;
                 if (i == 9) return SDError.InitError;
             }
+            if ((response_r1[0] & 0x7F) == 8) return SDError.CrcError;
             spi_port.read(&response_r7);
 
             time.sleep_ms(1);
+
+            // Turn ON CRC
+            spi_port.write(&CMD59);
+            for (0..10) |i| {
+                spi_port.read(&response_r1);
+                if ((response_r1[0] & 0x80) == 0) break;
+                if (i == 9) return SDError.InitError;
+            }
+            if ((response_r1[0] & 0x7F) == 8) return SDError.CrcError;
 
             // FIXME: sometimes fail to init
             // https://stackoverflow.com/questions/76002524/trying-to-initialize-sdhc-card-using-spi-after-sending-cmd55-in-preparation-for
@@ -109,6 +169,7 @@ pub fn SD_DRIVER(comptime spi_port: anytype, comptime cs_pin: anytype) type {
                     if ((response_r1[0] & 0x80) == 0) break;
                     if (i == 9) return SDError.InitError;
                 }
+                if ((response_r1[0] & 0x7F) == 8) return SDError.CrcError;
 
                 spi_port.write(&ACMD41);
                 for (0..10) |i| {
@@ -116,7 +177,11 @@ pub fn SD_DRIVER(comptime spi_port: anytype, comptime cs_pin: anytype) type {
                     if ((response_r1[0] & 0x80) == 0) break;
                     if (i == 9) return SDError.InitError;
                 }
-                if (response_r1[0] == 0) break;
+                switch (response_r1[0] & 0x7F) {
+                    0 => break,
+                    8 => return SDError.CrcError,
+                    else => {},
+                }
                 if (j == 1999) return SDError.InitError;
                 time.sleep_ms(1);
             }
@@ -129,6 +194,7 @@ pub fn SD_DRIVER(comptime spi_port: anytype, comptime cs_pin: anytype) type {
                 if ((response_r1[0] & 0x80) == 0) break;
                 if (i == 9) return SDError.InitError;
             }
+            if ((response_r1[0] & 0x7F) == 8) return SDError.CrcError;
             spi_port.read(&response_r3);
 
             spi_port.wait_complete();
@@ -153,11 +219,16 @@ pub fn SD_DRIVER(comptime spi_port: anytype, comptime cs_pin: anytype) type {
                 if ((response_r1[0] & 0xf0) == 0) return SDError.ReadError;
             }
             spi_port.read(buffer);
-            var crc: [2]u8 = undefined;
-            spi_port.read(&crc);
+            var crc_data: [2]u8 = undefined;
+            spi_port.read(&crc_data);
+
+            var crc = (@as(u16, crc_data[0]) << 8) | crc_data[1];
+            if (crc != crc16(buffer)) return SDError.CrcError;
         }
 
         pub fn read_single(addr: usize, buffer: []u8) SDError!void {
+            errdefer cleanup();
+
             cs_pin.put(0);
 
             var cmd = CMD17;
@@ -171,6 +242,7 @@ pub fn SD_DRIVER(comptime spi_port: anytype, comptime cs_pin: anytype) type {
                 if ((response_r1[0] & 0x80) == 0) break;
                 if (i == 9) return SDError.ReadError;
             }
+            if ((response_r1[0] & 0x7F) == 8) return SDError.CrcError;
 
             try read_data(buffer);
 
@@ -181,6 +253,8 @@ pub fn SD_DRIVER(comptime spi_port: anytype, comptime cs_pin: anytype) type {
         }
 
         pub fn read_multi(addr: usize, buffer: []u8) SDError!void {
+            errdefer cleanup();
+
             const count: usize = buffer.len / SECTOR_SIZE;
 
             cs_pin.put(0);
@@ -196,6 +270,7 @@ pub fn SD_DRIVER(comptime spi_port: anytype, comptime cs_pin: anytype) type {
                 if ((response_r1[0] & 0x80) == 0) break;
                 if (i == 9) return SDError.ReadError;
             }
+            if ((response_r1[0] & 0x7F) == 8) return SDError.CrcError;
 
             for (0..count) |i| {
                 try read_data(buffer[(SECTOR_SIZE * i)..(SECTOR_SIZE * (i + 1))]);
@@ -224,11 +299,17 @@ pub fn SD_DRIVER(comptime spi_port: anytype, comptime cs_pin: anytype) type {
             const data_token = [_]u8{token};
             spi_port.write(&data_token);
             spi_port.write(buffer);
-            const crc = [_]u8{ 0, 0 };
-            spi_port.write(&crc);
+
+            const crc = crc16(buffer);
+            const crc_data = [_]u8{ @as(u8, @truncate(crc >> 8)), @as(u8, @truncate(crc)) };
+            spi_port.write(&crc_data);
 
             spi_port.read(&response_r1);
-            if ((response_r1[0] & 0x1f) != 0b00101) return SDError.WriteError;
+            switch (response_r1[0] & 0x1f) {
+                0b00101 => {},
+                0b01011 => return SDError.CrcError,
+                else => return SDError.WriteError,
+            }
 
             // wait while busy
             while (true) {
@@ -239,6 +320,8 @@ pub fn SD_DRIVER(comptime spi_port: anytype, comptime cs_pin: anytype) type {
         }
 
         pub fn write_single(addr: usize, buffer: []u8) SDError!void {
+            errdefer cleanup();
+
             cs_pin.put(0);
 
             var cmd = CMD24;
@@ -252,6 +335,8 @@ pub fn SD_DRIVER(comptime spi_port: anytype, comptime cs_pin: anytype) type {
                 if ((response_r1[0] & 0x80) == 0) break;
                 if (i == 9) return SDError.ReadError;
             }
+            if ((response_r1[0] & 0x7F) == 8) return SDError.CrcError;
+
             //send dummy space
             spi_port.write(&[_]u8{0xff});
 
@@ -264,6 +349,8 @@ pub fn SD_DRIVER(comptime spi_port: anytype, comptime cs_pin: anytype) type {
         }
 
         pub fn write_multi(addr: usize, buffer: []u8) SDError!void {
+            errdefer cleanup();
+
             const count: usize = buffer.len / SECTOR_SIZE;
 
             cs_pin.put(0);
@@ -279,6 +366,7 @@ pub fn SD_DRIVER(comptime spi_port: anytype, comptime cs_pin: anytype) type {
                 if ((response_r1[0] & 0x80) == 0) break;
                 if (i == 9) return SDError.ReadError;
             }
+            if ((response_r1[0] & 0x7F) == 8) return SDError.CrcError;
 
             //send dummy space
             spi_port.write(&[_]u8{0xff});
@@ -308,6 +396,8 @@ pub fn SD_DRIVER(comptime spi_port: anytype, comptime cs_pin: anytype) type {
         }
 
         pub fn read_cid() SDError!u128 {
+            errdefer cleanup();
+
             var buffer: [16]u8 = undefined;
 
             cs_pin.put(0);
@@ -318,6 +408,7 @@ pub fn SD_DRIVER(comptime spi_port: anytype, comptime cs_pin: anytype) type {
                 if ((response_r1[0] & 0x80) == 0) break;
                 if (i == 9) return SDError.ReadError;
             }
+            if ((response_r1[0] & 0x7F) == 8) return SDError.CrcError;
 
             try read_data(&buffer);
 
@@ -335,6 +426,8 @@ pub fn SD_DRIVER(comptime spi_port: anytype, comptime cs_pin: anytype) type {
         }
 
         pub fn read_csd() SDError!u128 {
+            errdefer cleanup();
+
             var buffer: [16]u8 = undefined;
 
             cs_pin.put(0);
@@ -345,6 +438,7 @@ pub fn SD_DRIVER(comptime spi_port: anytype, comptime cs_pin: anytype) type {
                 if ((response_r1[0] & 0x80) == 0) break;
                 if (i == 9) return SDError.ReadError;
             }
+            if ((response_r1[0] & 0x7F) == 8) return SDError.CrcError;
 
             try read_data(&buffer);
 
@@ -396,6 +490,8 @@ pub fn SD_DRIVER(comptime spi_port: anytype, comptime cs_pin: anytype) type {
         }
 
         pub fn fix_block_len512() SDError!void {
+            errdefer cleanup();
+
             const size = try sector_size();
 
             if (size != 512) {
@@ -407,6 +503,7 @@ pub fn SD_DRIVER(comptime spi_port: anytype, comptime cs_pin: anytype) type {
                     if ((response_r1[0] & 0x80) == 0) break;
                     if (i == 9) return SDError.ReadError;
                 }
+                if ((response_r1[0] & 0x7F) == 8) return SDError.CrcError;
 
                 spi_port.wait_complete();
                 cs_pin.put(1);
